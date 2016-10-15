28105bf34051d2b78421fea2619d629f68d551ad
Samuele Malavasi <samuele@MacBook-Pro.fritz.box>
2016-10-15 19:25:58 +0200

    inserted getHeight and getWeight in HPReader

--- src/HealthProfileReader.java
+++ src/HealthProfileReader.java
@@ -1,11 +1,20 @@
-import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.Marshaller;
-import javax.xml.bind.Unmarshaller;
 
-import model.HealthProfile;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
 import model.Person;
 import dao.PeopleStore;
 
@@ -13,6 +22,8 @@ public class HealthProfileReader {
 	public static PeopleStore people = new PeopleStore();
 
 	public static void main(String[] args) throws Exception {
+		System.out.println(getHeight(7));
+		/*
 		JAXBContext jc = JAXBContext.newInstance(PeopleStore.class);
         System.out.println();
         System.out.println("Output from our XML File: ");
@@ -23,6 +34,58 @@ public class HealthProfileReader {
           System.out.println("Person: " + person.getFirstname() + " born "
               + person.getBirthdate());
         }
+        */
 
     }
+	
+	public static void printPeopleList(){
+		
+	}
+	
+	public static double getWeight(int id){
+		DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder = null;
+		try {
+		    builder = builderFactory.newDocumentBuilder();
+		    Document document = builder.parse(new FileInputStream("people.xml"));
+		    
+		    XPath xPath =  XPathFactory.newInstance().newXPath();
+		    return Double.parseDouble(xPath.compile("/people/peopleList/person[@id="+id+"]/healthprofile/weight").evaluate(document));
+		    }
+		catch (ParserConfigurationException e) {
+		    e.printStackTrace();  
+		}catch (SAXException e) {
+		    e.printStackTrace();
+		} catch (IOException e) {
+		    e.printStackTrace();
+		} catch (XPathExpressionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return -1;
+	}
+	
+	public static double getHeight(int id){
+		try {
+			DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder builder = builderFactory.newDocumentBuilder();
+		    Document document = builder.parse(new FileInputStream("people.xml"));
+		    XPath xPath =  XPathFactory.newInstance().newXPath();
+		    
+		    return Double.parseDouble(
+		    		xPath.compile("/people/peopleList/person[@id="+id+"]/healthprofile/height").evaluate(document)
+		    		);
+		    }
+		catch (ParserConfigurationException e) {
+		    e.printStackTrace();  
+		}catch (SAXException e) {
+		    e.printStackTrace();
+		} catch (IOException e) {
+		    e.printStackTrace();
+		} catch (XPathExpressionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return -1;
+	}	
 }
--- src/HealthProfileWriter.java
+++ src/HealthProfileWriter.java
@@ -1,6 +1,9 @@
 import java.io.File;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
 import java.util.List;
+
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
@@ -17,11 +20,39 @@ public class HealthProfileWriter {
 		Person pallo = new Person(new Long(1), "Pallo", "Pinco", "1984-06-21");
 		HealthProfile hp = new HealthProfile(68.0, 1.72);
 		Person john = new Person(new Long(2), "John", "Doe", "1985-03-20", hp);
-
 		people.getData().add(pallino);
 		people.getData().add(pallo);
 		people.getData().add(john);
+		
+		String[] names = {"Giuseppe","Maria","Giovanni","Anna","Antonio","Giuseppina","Mario","Rosa","Luigi","Angela","Francesco","Giovanna","Angelo","Teresa","Vincenzo","Lucia","Pietro","Carmela","Salvatore","Caterina"};
+		String[] surnames = {"Rossi","Russo","Ferrari","Esposito","Bianchi","Romano","Colombo","Ricci","Marino","Greco","Bruno","Gallo","Conti","De","Mancini","Costa","Giordano","Rizzo","Lombardi","Moretti"};
+		
+		GregorianCalendar gc = new GregorianCalendar();
+		int year, dayOfYear;
+		double weight, height;
+
+		
+		for(int c=0; c<20; c++){
+			year = randBetween(1900, 2016);
+			dayOfYear = randBetween(1, gc.getActualMaximum(gc.DAY_OF_YEAR));
+			weight = 10 + (Math.random() * 150); 
+			height = (50 +(Math.random() * 200))/100; 
+			
+			weight = Math.floor(weight * 100) / 100;
+			height = Math.floor(height * 100) / 100;
+			
+			gc.set(gc.YEAR, year);
+			gc.set(gc.DAY_OF_YEAR, dayOfYear);
+			
+			hp = new HealthProfile(weight, height);
+			Person p = new Person(new Long(c+3), names[c], surnames[c], gc.get(gc.YEAR) + "-" + (gc.get(gc.MONTH) + 1) + "-" + gc.get(gc.DAY_OF_MONTH), hp);
+			people.getData().add(p);
+		}
 	}	
+	
+    public static int randBetween(int start, int end) {
+        return start + (int)Math.round(Math.random() * (end - start));
+    }	
 
 	public static void main(String[] args) throws Exception {
 		
